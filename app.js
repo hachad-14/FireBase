7c5fcd1b354f3d28ac66cb9480d722f17b495ff5
hachad-14 <hachadsaad14@gmail.com>
2021-09-04 23:56:58 +0100

    test

--- app.js
+++ app.js
@@ -1,150 +1,64 @@
-/*you can have two buttons:
-one to call getPictureFromGallery();
-the other one to call getPictureFromCamera();
-Depnding on which the user clicks, the appropriate action will take place.
-Note that getPicturefromCamera() may crash the app in some phones due to memory issues.
-This code does not fix that issue, do well to send a pull request.
-*/
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+//import { getStorage } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-storage.js";
+var ImgName, ImgUrl;
+var files = [];
+var reader;
+const firebaseConfig = {
+  apiKey: "AIzaSyBsWbH_cHvDHsMFiMi_DNn13e7sfwk2zZA",
+  authDomain: "test-54a77.firebaseapp.com",
+  databaseURL: "https://test-54a77-default-rtdb.firebaseio.com",
+  projectId: "test-54a77",
+  storageBucket: "test-54a77.appspot.com",
+  messagingSenderId: "94297928022",
+  appId: "1:94297928022:web:f82973978d5592cc47a6f7",
+  measurementId: "G-12GPLK769J"
+};
+firebase.initializeApp(firebaseConfig);
+//const firebase = initializeApp(firebaseConfig);
+//const storage = getStorage(firebase);
 
-function getPictureFromGallery(){
-
-    navigator.camera.getPicture(onSuccess, onFail, { 
-        destinationType: Camera.DestinationType.FILE_URI,
-        sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
-        saveToPhotoAlbum: true,
-        allowEdit: true,
-        quality: 100,
-        correctOrientation: true,
-
+document.getElementById("select").onclick = function(e) {
+  var input = document.createElement('input');
+  input.type = 'file';
+  //input.capture = 'user' or 'environement'
+  document.getElementById('console').innerHTML = 'Press Fired';
+  input.addEventListener('change', (e) => {
+    document.getElementById('console').innerHTML = 'Image Fired';
+    files = e.target.files;
+    reader = new FileReader();
+    reader.onload = function() {
+      document.getElementById("myimg").src = files.result;
+      $('#myimg').attr('src', reader.result);
+      }
+      reader.readAsDataURL(files[0])
+  });
+  input.click();
+}
+document.getElementById("upload").onclick = function() {
+  ImgName = document.getElementById('namebox').value;
+  var uploadTask = firebase.storage().ref('Images/'+ImgName+".png").put(files[0]);
+  uploadTask.on('state_changed', function(snapshot) {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      document.getElementById("upProgress").innerHTML = 'Upload'+' '+progress+'%';
+  },
+  function(error) {
+      alert('error');
+  },
+  function() {
+      uploadTask.snapshot.ref.getDownloadURL().then(function(url) {
+          ImgUrl = url;
+      firebase.database().ref('Pictures/'+ImgName).set({
+          Name: ImgName,
+          Link: ImgUrl
+      });
+      console.log('Image Stored In DataBase.');
+      document.getElementById('console').innerHTML = 'Image Stored In DataBase';
     });
-
-
+  });
 }
-
-function getPictureFromCamera(){
-
-    navigator.camera.getPicture(onSuccess, onFail, { 
-        destinationType: Camera.DestinationType.FILE_URI,
-        sourceType : Camera.PictureSourceType.CAMERA,
-        saveToPhotoAlbum: true,
-        allowEdit: true,
-        quality: 100,
-        correctOrientation: true,
-
-    });
-
-
-}
-
-
-
-
-        function onSuccess(imageData) {
-            var image = document.getElementById('myImage');
-            image.src=  imageData;
-
-
-var file = imageData; //useless idiot like me :) 
-
-alert("File content: "+ file);
-// Create the file metadata
-var metadata = {
-contentType: 'image/jpeg'
-};
-
-
-var fileName = file.substring(file.lastIndexOf('/')+1); //get name of file from file path
-
-
-var storageRef = firebase.storage().ref();
-
-
-
-
-
-var getFileBlob = function (url, cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.responseType = "blob";
-    xhr.addEventListener('load', function() {
-        cb(xhr.response);
-    });
-    xhr.send();
-};
-
-var blobToFile = function (blob, name) {
-    blob.lastModifiedDate = new Date();
-    blob.name = name;
-    return blob;
-};
-
-var getFileObject = function(filePathOrUrl, cb) {
-   getFileBlob(filePathOrUrl, function (blob) {
-       
-      cb(blobToFile(blob, fileName));
-   });
-};
-
-getFileObject(imageData, function (fileObject) {
-
-
-var timeStamp = Math.floor(Date.now() / 1000);
-// Upload file and metadata to the object 'images/mountains.jpg'
-var uploadTask = storageRef.child(localStorage.myFirebase_user_id+"/"+timeStamp+"/"+ fileName).put(fileObject);
-
-// Listen for state changes, errors, and completion of the upload.
-uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-function(snapshot) {
-// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-console.log('Upload is ' + progress + '% done');
-switch (snapshot.state) {
-  case firebase.storage.TaskState.PAUSED: // or 'paused'
-  console.log('Upload is paused');
-  break;
-  case firebase.storage.TaskState.RUNNING: // or 'running'
-  console.log('Upload is running');
-  break;
-}
-}, function(error) {
-switch (error.code) {
-    case 'storage/unauthorized':
-  // User doesn't have permission to access the object
-  alert("Storage unauthorized");
-  break;
-
-  case 'storage/canceled':
-  // User canceled the upload
-  alert("Error: storage/canceled");
-  break;
-
-  case 'storage/unknown':
-  // Unknown error occurred, inspect error.serverResponse
-  alert("Error: storage/unknown");
-  break;
-}
-}, function() {
-// Upload completed successfully, now we can get the download URL
-var downloadURL = uploadTask.snapshot.downloadURL;
-console.log("The download URL: "+downloadURL);
-
-//this is where you have to enter the code to update any desired firebase table with the image url
-//eg this below code will add a 'profilePhoto' field and record to 'users' table for the user with the id of userId
-//read more about saving data in firebase here: https://firebase.google.com/docs/database/web/save-data
-
-firebase.database().ref('users/' + userId).update({
-profilePhoto: downloadURL
-});
-
-});
-
-
-}); 
-
-}
-function onFail(message) {
-//alert('Failed because: ' + message);
-setTimeout(function(){
-navigator.notification.alert(message); //show a pop up message if something failed in the process eg.image upload canceled 
-}, 0);
+document.getElementById('retrieve').onclick = function() {
+  ImgName = document.getElementById('namebox').value;
+  firebase.database().ref('Pictures/'+ImgName).on('value', function(snapshot){
+    document.getElementById('myimg').src = snapshot.val().Link;
+  });
 }
