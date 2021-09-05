2fafacad3ce9615bb6309bf40c6ccff40fd850a3
hachad-14 <hachadsaad14@gmail.com>
2021-09-05 21:52:13 +0100

    test

--- app.js
+++ app.js
@@ -34,31 +34,31 @@ document.getElementById("select").onclick = function(e) {
     input.click();
 }
 document.getElementById("upload").onclick = function() {
-    if(document.getElementById("namebox").value.length == 0) {
-        ImgName = document.getElementById("myimg").src.split("/").pop()[0];
-        console.log(ImgName);
-    } else {
-        ImgName = document.getElementById('namebox').value;
-    }
-    var uploadTask = firebase.storage().ref('Images/'+ImgName+".png").put(files[0]);
-    uploadTask.on('state_changed', function(snapshot) {
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        document.getElementById("upProgress").innerHTML = 'Upload'+' '+progress+'%';
-    },
-    function(error) {
-        alert('error');
-    },
-    function() {
-        uploadTask.snapshot.ref.getDownloadURL().then(function(url) {
-            ImgUrl = url;
-        firebase.database().ref('Pictures/'+ImgName).set({
-            Name: ImgName,
-            Link: ImgUrl
-        });
-        console.log('Image Stored In DataBase.');
-        document.getElementById('console').innerHTML = 'Image Stored In DataBase';
+  if(document.getElementById("namebox").value.length == 0) {
+    ImgName = 'post' + Math.floor(1000 + Math.random() * 9000);
+    console.log(ImgName + '.png');
+  } else {
+    ImgName = document.getElementById('namebox').value;
+  }
+  var uploadTask = firebase.storage().ref('Images/'+ImgName+".png").put(files[0]);
+  uploadTask.on('state_changed', function(snapshot) {
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    document.getElementById("console").innerHTML = 'Upload'+' '+progress+'%';
+  },
+  function(error) {
+    alert('error');
+  },
+  function() {
+    uploadTask.snapshot.ref.getDownloadURL().then(function(url) {
+      ImgUrl = url;
+      firebase.database().ref('Pictures/'+ImgName).set({
+          Name: ImgName,
+          Link: ImgUrl
       });
+      console.log('Image Stored In DataBase.');
+      document.getElementById('console').innerHTML = 'Image Stored In DataBase';
     });
+  });
 }
 document.getElementById('retrieve').onclick = function() {
   ImgName = document.getElementById('namebox').value;
@@ -67,3 +67,9 @@ document.getElementById('retrieve').onclick = function() {
     $('#myimg').attr('src', snapshot.val().Link);
   });
 }
+
+var increment = (function(n) {
+  return function() {
+    console.log(Math.floor(1000 + Math.random() * 9000));
+  }
+});
